8b8343e033b0845effe1fc4e80c3374fb2edc05a
Demétrio Alves Atra <datra@verg1759.lobtec.com.br>
2022-07-13 18:01:33 -0300

    v. 1.0.6

--- client/src/pages/SignIn/index.js
+++ client/src/pages/SignIn/index.js
@@ -1,27 +1,24 @@
-import { Container, Button, TextField, Grid } from '@mui/material'
+import { Container, Button, TextField, Grid, Paper } from '@mui/material'
+import './index.css'
 
 function SignIn() {
-    return (
-        <Container className='container'>
-            <Grid
-            container
-            spacing={2}
-            direction='column'
-            style={{minHeight: '100vh'}}>
-                <Grid item>
-                    <TextField type='email' fullWidth label='E-mail' />
-                </Grid>
-                <Grid item>
-                    <TextField type='password' fullWidth label='Password' />
-                </Grid>
-                <Grid item>
-                    <Button variant='contained' fullWidth>Login</Button>
-                </Grid>
-            </Grid>
+    const paperStyle = {padding: 20, height: '60vh', width: 380, margin: '80px auto'}
 
-        
-            
-        </Container>
+    return (
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align="center">
+                    <h1>Fisiotech</h1>
+                    <h2>Sign In</h2>
+                </Grid>
+                <Grid container spacing={2} direction="column" style={{marginTop: 20}}>
+                    <Grid item><TextField label="Email" fullWidth required/></Grid>
+                    <Grid item><TextField label="Password" fullWidth required/></Grid>
+                    <Grid item><Button variant='contained' fullWidth>SignIn</Button></Grid>
+                    <Grid item><Button variant='contained' fullWidth>Google</Button></Grid>
+                </Grid>
+            </Paper>
+        </Grid>
     )
 }
 
