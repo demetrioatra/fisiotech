2c3a92e11c16516ced72e21d6c446a50473e1ecf
Demétrio Alves Atra <datra@verg1759.lobtec.com.br>
2022-07-12 22:19:16 -0300

    v. 1.0.3

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,11 @@
-import { useState, useEffect } from 'react'
 import RoutesApp from './routes'
 import './App.css'
 
-function App () {
-  const [pacientes, setPaciente] = useState([])
-
-  useEffect(() => {
-    function loadPacientes () {
-      let getRoute = 'http://localhost:3500/pacientes'
-      
-      fetch(getRoute)
-        .then((res) => res.json())
-        .then((json) => {
-          console.log(json)
-        })
-    }
-    loadPacientes()
-  }, [])
-
+function App() {
   return (
-    <>
+    <div className = 'App'>
       <RoutesApp />
-    </>
+    </div>
   )
 }
 
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,34 @@
+import { useState, useEffect } from 'react'
 import './index.css'
 
 function Home() {
+    const [pacientes, setPaciente] = useState([])
+
+    useEffect(() => {
+        function loadPacientes() {
+            let getRoute = 'http://localhost:3500/pacientes'
+
+            fetch(getRoute)
+                .then((res) => res.json())
+                .then((json) => {
+                    setPaciente(json)
+                })
+        }
+        loadPacientes()
+    }, [])
+
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <div className = "container">
+            {
+                pacientes.map((p) => {
+                    return (
+                        <ul key = { p.id }>
+                            <li>{ p.nome } - { p.telefone } - { p.idade } - { p.email }</li>
+                        </ul>
+                    )
+                })
+            }
+        </div>
     )
 }
 
