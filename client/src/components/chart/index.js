c3e5367a244895d74fd7d27b71624bd7e0e8b995
Demétrio Alves Atra <datra@verg1759.lobtec.com.br>
2022-07-14 10:35:23 -0300

    v. 1.1.2

--- client/src/components/chart/index.js
+++ client/src/components/chart/index.js
@@ -1,9 +1,49 @@
-function Chart() {
-    return (
-        <>
-            <h1>Chart</h1>
-        </>
-    )
-}
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import {
+  Chart,
+  PieSeries,
+  Title,
+} from '@devexpress/dx-react-chart-material-ui';
+import { Animation } from '@devexpress/dx-react-chart';
+import './index.css'
 
-export default Chart
+const data = [
+  { patologia: '', area: 12 },
+  { patologia: '', area: 7 },
+  { patologia: '', area: 7 },
+  { patologia: '', area: 7 },
+  { patologia: '', area: 6 },
+  { patologia: '', area: 5 },
+];
+export default class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data,
+    };
+  }
+
+  render() {
+    const { data: chartData } = this.state;
+
+    return (
+      <Paper className='chartContainer'>
+        <Chart
+          data={chartData}
+        >
+          <PieSeries
+            valueField="area"
+            argumentField="patologia"
+            text='text'
+          />
+          <Title
+            text="Patologias atendidas"
+          />
+          <Animation />
+        </Chart>
+      </Paper>
+    );
+  }
+}
