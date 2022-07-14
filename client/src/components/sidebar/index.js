249084b7434dc1e5ab5cba57906451e70cab61b7
Demétrio Alves Atra <datra@verg1759.lobtec.com.br>
2022-07-14 11:00:23 -0300

    v. 1.1.3

--- client/src/components/sidebar/index.js
+++ client/src/components/sidebar/index.js
@@ -3,7 +3,6 @@ import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -31,22 +30,8 @@ export default function PermanentDrawerLeft() {
                 anchor="left"
             >
                 <Toolbar />
-                <Divider />
                 <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItemButton>
-                    </ListItem>
-                ))}
-                </List>
-                <Divider />
-                <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                {['Dashboard', 'Pacientes', 'Usuários', 'Login', 'Register', 'Not Found'].map((text, index) => (
                     <ListItem key={text} disablePadding>
                     <ListItemButton>
                         <ListItemIcon>
