287746828a4011e84e290e6318f7a5b6e5f0750f
Demétrio Alves Atra <datra@verg1759.lobtec.com.br>
2022-07-14 13:58:57 -0300

    v. 1.1.7

--- client/src/pages/SignIn/index.js
+++ client/src/pages/SignIn/index.js
@@ -1,20 +1,22 @@
-import { Container, Button, TextField, Grid, Paper } from '@mui/material'
+import { Button, TextField, Grid, Paper } from '@mui/material'
+import SpaIcon from '@mui/icons-material/Spa';
 import './index.css'
 
 function SignIn() {
-    const paperStyle = {padding: 20, height: '60vh', width: 380, margin: '80px auto'}
+    const paperStyle = {padding: 20, height: '70vh', width: 380, margin: '0 auto'}
 
     return (
-        <Grid>
-            <Paper elevation={10} style={paperStyle}>
-                <Grid align="center">
-                    <h1>Fisiotech</h1>
-                    <h2>Sign In</h2>
-                </Grid>
+        <Grid align="center">
+            <Grid>
+                <h1 className='logo'><SpaIcon className='icon' /> Fisiotech</h1>
+            </Grid>
+            <Paper elevation={2} style={paperStyle}>
+                <h2>Sign In to continue</h2>
                 <Grid container spacing={2} direction="column" style={{marginTop: 20}}>
                     <Grid item><TextField label="Email" fullWidth required/></Grid>
                     <Grid item><TextField label="Password" fullWidth required/></Grid>
-                    <Grid item><Button variant='contained' fullWidth>SignIn</Button></Grid>
+                    <Grid item><Button variant='contained' fullWidth>Sign In</Button></Grid>
+                    <Grid item><p>OR</p></Grid>
                     <Grid item><Button variant='contained' fullWidth>Google</Button></Grid>
                 </Grid>
             </Paper>
