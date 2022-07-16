8523f5b431b385c650f8cbee0eb012f6ccb4c10a
Antônio Carlos Veloso Pinheiro <apinheiro@verg1759.lobtec.com.br>
2022-07-16 13:10:55 -0300

    v. 1.3.4

--- server/models/UsuarioModel.js
+++ server/models/UsuarioModel.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
+const opts = { toJSON: { virtuals: true } };
+
 
 // Schema de usuário
 const Schema = new mongoose.Schema ({
     email: String,
     password: String
-})
+}, opts)
 
 // Export do schema de usuário
 module.exports = mongoose.model('Usuario', Schema)
