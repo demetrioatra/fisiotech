4527aabf8260325309b4a3f91e822869f2aea9b2
Demétrio Alves Atra <datra@verg1759.lobtec.com.br>
2022-07-16 20:52:02 -0300

    v. 1.4.0

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,14 +1,9 @@
-import { Grid } from '@mui/material'
-import SideBar from '../../components/sidebar'
-import Header from '../../components/header'
 import Chart from '../../components/chart'
 import './index.css'
 
 export default function Home() {
     return (
         <>
-            <Header  />
-            <SideBar />
             <Chart />
         </>
     )
